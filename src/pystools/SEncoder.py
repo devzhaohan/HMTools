259198587b1e86e1ff15a416fd1bdaafb87a0914
steve <zhoan@163.com>
2023-07-02 13:16:51 +0800

    feat: SEncoder 增加通过时间戳生成 id 的方法

--- src/pystools/SEncoder.py
+++ src/pystools/SEncoder.py
@@ -1,4 +1,6 @@
 import hashlib
+import random
+import time
 import uuid
 import shortuuid
 
@@ -31,3 +33,37 @@ class SEncoder:
         if to_upper:
             return m.hexdigest().upper()
         return m.hexdigest()
+
+    @staticmethod
+    def gen_num_str_by_timestamp(length=8):
+        sleep_time = random.random() * 0.8
+        print("sleep time: ", sleep_time)
+        time.sleep(sleep_time)
+        t = time.time()
+        tm = t * 1000 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000
+        # 显示完整的数字，不用科学计数法
+        tstr = "{:.0f}".format(tm)
+        # print(tstr)
+        nameStr = tstr[13:-1]
+
+        def split_string(string, cnt=4):
+            length = len(string)
+            segment_length = length // cnt
+            segments = []
+            for i in range(cnt):
+                start = i * segment_length
+                end = (i + 1) * segment_length
+                segment = string[start:end]
+                segments.append(segment)
+            return segments
+
+        segments = split_string(nameStr, length)
+        # print(segments)
+
+        lid = ""
+        for segment in segments:
+            # 从 segment 中随机取一个字符
+            char = random.choice(segment)
+            # 把字符添加到 lid 中
+            lid += char
+        return lid
